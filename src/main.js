9b2c063270e7324e8d33ce9812aa9404c5a5b72d
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-15 22:27:36 +0100

    Adding connection

--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueAuth from '@websanova/vue-auth'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
+
+Vue.axios.defaults.baseURL = 'http://localhost:8000/api'
+
+Vue.router=router
+Vue.use(VueAuth, {
+  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
+})
 /* eslint-disable no-new */
-new Vue({
+window.App=new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  components: {App},
+  template: '<App/>',
+  mounted(){
+    this.$auth.login({
+      method: 'GET',
+      url: 'login'
+    })
+  }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Master from '../layout/Master'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Master',
+      component: Master,
+      meta: {
+        auth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     }
-  ]
+  ],
 })
+
+router.beforeEach((to, from, next) => {
+  const authRequired = to.matched.some((route) => route.meta.auth)
+  console.log(authRequired)
+  const authed = false
+  if (authRequired && !authed) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
